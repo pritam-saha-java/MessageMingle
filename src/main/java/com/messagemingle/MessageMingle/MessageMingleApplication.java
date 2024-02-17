b00b815d1eb22a37c0d4067165b968f03536f49e
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-18 00:19:01 +0530

    initial commit

---
+++ src/main/java/com/messagemingle/MessageMingle/MessageMingleApplication.java
@@ -0,0 +1,13 @@
+package com.messagemingle.MessageMingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageMingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageMingleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messagemingle/MessageMingle/MessageMingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagemingle.MessageMingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
