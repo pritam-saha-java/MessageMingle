83a9fda2ffda47422bd01bd769bb706c9302ab6a
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-18 16:58:32 +0530

    Add new repository methods

--- src/main/java/com/messagemingle/MessageMingle/repository/ChatRoomRepository.java
+++ src/main/java/com/messagemingle/MessageMingle/repository/ChatRoomRepository.java
@@ -2,9 +2,11 @@ package com.messagemingle.MessageMingle.repository;
 
 import com.messagemingle.MessageMingle.entity.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, String> {
 
     Optional<ChatRoomEntity> findBySenderIdAndRecipientId(String senderId, String recipientId);
--- src/main/java/com/messagemingle/MessageMingle/repository/UserRepository.java
+++ src/main/java/com/messagemingle/MessageMingle/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.messagemingle.MessageMingle.repository;
 import com.messagemingle.MessageMingle.entity.UserEntity;
 import com.messagemingle.MessageMingle.utility.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     List<UserEntity> findAllByStatus(Status status);
 }
